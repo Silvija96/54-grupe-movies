9f67280fa425d488a7c489435417de6ae76807a6
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-05-22 15:03:24 +0300

    up

--- src/pages/public/PageLogin.js
+++ src/pages/public/PageLogin.js
@@ -1,36 +1,36 @@
-import { PageTemplate } from "../templates/PageTemplate.js";
+import { NODE_ENV } from "../../env.js";
+import { PageTemplate } from "../../templates/PageTemplate.js";
 
-export class PageRegister extends PageTemplate {
+export class PageLogin extends PageTemplate {
     constructor(req) {
         super(req);
-        this.pageJS = 'register';
-        this.activeMenuIndex = 4;
+        this.pageJS = 'login';
+        this.activeMenuIndex = 3;
     }
 
     async main() {
+        const email = NODE_ENV === 'dev' ? 'chuck@norris.lt' : '';
+        const password = NODE_ENV === 'dev' ? 'chuck@norris.lt' : '';
+
         return `
             <main>
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-md-10 col-lg-6 col-xl-5 col-xxl-4">
-                            <h1 class="display-1">Register</h1>
+                            <h1 class="display-1">Login</h1>
                             <div id="error" class="alert alert-danger d-none" role="alert">
                                 A simple danger alert—check it out!
                             </div>
                             <form>
                                 <div class="form-floating">
-                                    <input id="email" type="email" class="form-control" placeholder="name@example.com">
+                                    <input type="email" value="${email}" class="form-control" id="email" placeholder="name@example.com">
                                     <label for="email">Email address</label>
                                 </div>
                                 <div class="form-floating">
-                                    <input id="password" type="password" class="form-control" placeholder="Password">
+                                    <input type="password" value="${password}" class="form-control" id="password" placeholder="Password">
                                     <label for="password">Password</label>
                                 </div>
-                                <div class="form-check text-start my-3">
-                                    <input class="form-check-input" type="checkbox" value="remember-me" id="tos">
-                                    <label class="form-check-label" for="tos">Agree with Terms of Service</label>
-                                </div>
-                                <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
+                                <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
                             </form>
                         </div>
                     </div>
--- src/pages/public/PageRegister.js
+++ src/pages/public/PageRegister.js
@@ -1,10 +1,10 @@
-import { PageTemplate } from "../templates/PageTemplate.js";
+import { PageTemplate } from "../../templates/PageTemplate.js";
 
-export class PageLogin extends PageTemplate {
+export class PageRegister extends PageTemplate {
     constructor(req) {
         super(req);
-        this.pageJS = 'login';
-        this.activeMenuIndex = 3;
+        this.pageJS = 'register';
+        this.activeMenuIndex = 4;
     }
 
     async main() {
@@ -13,20 +13,24 @@ export class PageLogin extends PageTemplate {
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-md-10 col-lg-6 col-xl-5 col-xxl-4">
-                            <h1 class="display-1">Login</h1>
+                            <h1 class="display-1">Register</h1>
                             <div id="error" class="alert alert-danger d-none" role="alert">
                                 A simple danger alert—check it out!
                             </div>
                             <form>
                                 <div class="form-floating">
-                                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                                    <input id="email" type="email" class="form-control" placeholder="name@example.com">
                                     <label for="email">Email address</label>
                                 </div>
                                 <div class="form-floating">
-                                    <input type="password" class="form-control" id="password" placeholder="Password">
+                                    <input id="password" type="password" class="form-control" placeholder="Password">
                                     <label for="password">Password</label>
                                 </div>
-                                <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
+                                <div class="form-check text-start my-3">
+                                    <input class="form-check-input" type="checkbox" value="remember-me" id="tos">
+                                    <label class="form-check-label" for="tos">Agree with Terms of Service</label>
+                                </div>
+                                <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
                             </form>
                         </div>
                     </div>

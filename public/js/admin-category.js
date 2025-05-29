b4f456855d3e13fab75de51982408f9ce7be9d45
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-05-29 15:13:20 +0300

    up

---
+++ public/js/admin-category.js
@@ -0,0 +1,31 @@
+const formDOM = document.forms[0];
+const nameDOM = document.getElementById('name');
+const urlDOM = document.getElementById('url');
+const descriptionDOM = document.getElementById('description');
+
+if (formDOM) {
+    formDOM.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const data = {
+            name: nameDOM.value,
+            url: urlDOM.value,
+            description: descriptionDOM.value,
+            status: document.querySelector('[name="status"]:checked').value,
+        };
+
+        fetch(formDOM.action, {
+            method: formDOM.dataset.method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(data => data.json())
+            .then(data => {
+
+                console.log(data);
+            })
+            .catch(err => console.log(err));
+    });
+}
